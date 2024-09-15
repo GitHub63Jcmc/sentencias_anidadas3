d769cac40388402b39996ce2bb0745413119b34f
Joao Cabral de Matos Carvalho <jcmcgoojcmc@gmail.com>
2024-09-16 01:53:03 +0200

    Cambios en script 1

--- js/sentencias_anidadas03.js
+++ js/sentencias_anidadas03.js
@@ -7,33 +7,33 @@ function iniciarFunciones() {
     let resultado;
 
     function funcionA(num1, num2) {
-        alert("Has elegido SUMA");
+        alert(`Has elegido SUMAR ${num1} y ${num2}`);
         resultado = num1 + num2;
-        document.getElementById("la_p2").innerHTML = `La suma de ${num1} y ${num2} = ${resultado}`; // Actualizar el contenido del <p>
+        document.getElementById("la_p1").innerHTML = `La suma de ${num1} y ${num2} son 👉${resultado}`; // Actualizar el contenido del <p>
     }
     
     function funcionB(num1, num2) {
-        alert("Has elegido RESTA");
+        alert(`Has elegido RESTA de ${num1} menos ${num2}`);
         resultado = num1 - num2;
-        document.getElementById("la_p2").innerHTML = `La resta de ${num1} y ${num2} = ${resultado}`; // Actualizar el contenido del <p>
+        document.getElementById("la_p2").innerHTML = `La resta de ${num1} y ${num2} son 👉 ${resultado}`; // Actualizar el contenido del <p>
     }
     
     function funcionC(num1, num2) {
-        alert("Has elegido MULTIPLICACIÓN");
+        alert(`Has elegido MULTIPLICACIÓN de ${num1} por ${num2}`);
         resultado = num1 * num2;
-        document.getElementById("la_p2").innerHTML = `La multiplicacion de ${num1} y ${num2} = ${resultado}`; // Actualizar el contenido del <p>
+        document.getElementById("la_p3").innerHTML = `La multiplicacion de ${num1} y ${num2} son 👉 ${resultado}`; // Actualizar el contenido del <p>
     }
     
     function funcionD(num1, num2) {
-        alert("Has elegido DIVISIÓN");
+        alert(`Has elegido la DIVISIÓN de ${num1} entre ${num2}`);
         resultado = num1 / num2;
-        document.getElementById("la_p2").innerHTML = `La division de ${num1} y ${num2} = ${resultado}`; // Actualizar el contenido del <p>
+        document.getElementById("la_p4").innerHTML = `La division de ${num1} y ${num2} son 👉 ${resultado}`; // Actualizar el contenido del <p>
     }
 
     function funcionE(num1, num2) {
-        alert("Has elegido RESTA DE LA DIVISIÓN");
+        alert(`Has elegido RESTA DE LA DIVISIÓN de ${num1} y ${num2}`);
         resultado = num1 % num2;
-        document.getElementById("la_p2").innerHTML = `La resta de la division de ${num1} y ${num2} = ${resultado} -> Es la resta de la División`; // Actualizar el contenido del <p>
+        document.getElementById("la_p5").innerHTML = `La resta de la division de ${num1} y ${num2} son 👉 ${resultado}`; // Actualizar el contenido del <p>
     }
     
     switch (operacion) {
